5de994d175c21d3a45ac23386be089d514a29419
Gonzalo Martínez <martinezgonzalo6cel@gmail.com>
2020-10-25 18:41:56 -0300

    Added IPrinter and ConsolePrinter

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -25,12 +25,12 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
-        public void PrintRecipe()
+        public void PrintRecipe(IPrinter printer)
         {
-            Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
+            printer.Print($"Receta de {this.FinalProduct.Description}:");
             foreach (Step step in this.steps)
             {
-                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                printer.Print($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
         }
