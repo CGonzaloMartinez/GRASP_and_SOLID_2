d2f51a6ad77ecaba393c4c91695544eb41904002
TyMata <alvaromanuel.machado@correo.ucu.edu.uy>
2020-10-25 19:40:21 -0300

    Se corrigen faltas de ortografia

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -6,8 +6,8 @@ namespace Full_GRASP_And_SOLID.Library
         Se define la interfaz IPrinter para que cualquier clase que implemente esta interfaz
         pueda ser intercambiada con ConsolePrinter y permitir imprimir el texto sin afectar
         el codigo de ConsolePrinter(LSP).
-        Tambien esta interfaz permite agregar nuevas maneras de imporimir sin modificar el 
-        codigo ya existente, asi cumplendo con el principio OCP.
+        Tambien esta interfaz permite agregar nuevas maneras de imprimir sin modificar el 
+        codigo ya existente, asi cumpliendo con el principio OCP.
     */
     public interface IPrinter
     {
