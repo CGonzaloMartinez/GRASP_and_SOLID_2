39aaac82dd966adfc200c539e30c918f4d56d5fa
Gonzalo Martínez <martinezgonzalo6cel@gmail.com>
2020-10-25 19:09:13 -0300

    Justificacion ConsolePrinter

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -3,6 +3,10 @@ using System;
 
 namespace Full_GRASP_And_SOLID.Library
 {
+    /*
+        La clase ConsolePrinter cumple con el principio SRP, pues su única responsabilidad
+        es la de imprimir por pantalla el texto que se le provea.
+    */
     public class ConsolePrinter : IPrinter
     {
         public void Print(string stringToPrint)
