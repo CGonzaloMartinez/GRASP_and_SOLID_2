e3fc574cc0f8884d6ce77734aa5ad4b6cd320a36
TyMata <alvaromanuel.machado@correo.ucu.edu.uy>
2020-10-25 19:11:49 -0300

    Comentarios y cambios
    Se agregan comentarios en la interfaz IPrinter y en la clase Recipe.
    Se realizaron cambios en el metodo PrintRecipe que ahora se llama GetTextToPrint

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -2,6 +2,13 @@
 
 namespace Full_GRASP_And_SOLID.Library
 {
+    /*
+        Se define la interfaz IPrinter para que cualquier clase que implemente esta interfaz
+        pueda ser intercambiada con ConsolePrinter y permitir imprimir el texto sin afectar
+        el codigo de ConsolePrinter(LSP).
+        Tambien esta interfaz permite agregar nuevas maneras de imporimir sin modificar el 
+        codigo ya existente, asi cumplendo con el principio OCP.
+    */
     public interface IPrinter
     {
         void Print(string textToPrint);
